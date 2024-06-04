36c6c4a94a7b115811a31de4b0faf87588a7c4e9
one-melon <73514446+one-melon@users.noreply.github.com>
2024-06-04 13:21:28 +0800

    2024.6.4

--- SGCLDGA-main/model.py
+++ SGCLDGA-main/model.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 import numpy as np
 
 class SGCLDGA(nn.Module):
-    def __init__(self, n_u, n_i, d, u_mul_s, v_mul_s, ut, vt, train_csr, adj_norm, l, temp, lambda_1, lambda_2, dropout,
+    def __init__(self, n_u, n_i, d, g_mul_s, v_mul_s, ut, vt, train_csr, adj_norm, l, temp, lambda_1, lambda_2, dropout,
                  batch_user, device):
         super(SGCLDGA, self).__init__()
 
@@ -30,12 +30,12 @@ class SGCLDGA(nn.Module):
         self.E_d_list = [None] * (l + 1)
         self.E_g_list[0] = self.E_g_0
         self.E_d_list[0] = self.E_d_0
-        self.Z_u_list = [None] * (l + 1)
-        self.Z_i_list = [None] * (l + 1)
-        self.G_u_list = [None] * (l + 1)
-        self.G_i_list = [None] * (l + 1)
-        self.G_u_list[0] = self.E_g_0
-        self.G_i_list[0] = self.E_d_0
+        self.Z_g_list = [None] * (l + 1)
+        self.Z_d_list = [None] * (l + 1)
+        self.G_g_list = [None] * (l + 1)
+        self.G_d_list = [None] * (l + 1)
+        self.G_g_list[0] = self.E_g_0
+        self.G_d_list[0] = self.E_d_0
         self.temp = temp
         self.lambda_1 = lambda_1
         self.lambda_2 = lambda_2
@@ -46,7 +46,7 @@ class SGCLDGA(nn.Module):
         self.E_g = None
         self.E_d = None
 
-        self.u_mul_s = u_mul_s
+        self.g_mul_s = g_mul_s
         self.v_mul_s = v_mul_s
         self.ut = ut
         self.vt = vt
@@ -60,45 +60,45 @@ class SGCLDGA(nn.Module):
         else:  # training phase
             for layer in range(1, self.l + 1):
                 # GNN propagation
-                self.Z_u_list[layer] = (
+                self.Z_g_list[layer] = (
                     torch.spmm(sparse_dropout(self.adj_norm, self.dropout), self.E_d_list[layer - 1]))
-                self.Z_i_list[layer] = (
+                self.Z_d_list[layer] = (
                     torch.spmm(sparse_dropout(self.adj_norm, self.dropout).transpose(0, 1), self.E_g_list[layer - 1]))
 
                 # svd_adj propagation
                 vt_ei = self.vt @ self.E_d_list[layer - 1]
-                self.G_u_list[layer] = (self.u_mul_s @ vt_ei)
+                self.G_g_list[layer] = (self.g_mul_s @ vt_ei)
                 ut_eu = self.ut @ self.E_g_list[layer - 1]
-                self.G_i_list[layer] = (self.v_mul_s @ ut_eu)
+                self.G_d_list[layer] = (self.v_mul_s @ ut_eu)
 
                 # aggregate
-                self.E_g_list[layer] = self.Z_u_list[layer]
-                self.E_d_list[layer] = self.Z_i_list[layer]
+                self.E_g_list[layer] = self.Z_g_list[layer]
+                self.E_d_list[layer] = self.Z_d_list[layer]
 
-            self.G_u = sum(self.G_u_list)
-            self.G_i = sum(self.G_i_list)
+            self.G_u = sum(self.G_g_list)
+            self.G_i = sum(self.G_d_list)
 
             # aggregate across layers
             self.E_g = sum(self.E_g_list)
             self.E_d = sum(self.E_d_list)
 
             # cl loss
-            G_u_norm = self.G_u
+            G_g_norm = self.G_u
             E_g_norm = self.E_g
-            G_i_norm = self.G_i
+            G_d_norm = self.G_i
             E_d_norm = self.E_d
-            neg_score = torch.log(torch.exp(G_u_norm[uids] @ E_g_norm.T / self.temp).sum(1) + 1e-8).mean()
-            neg_score += torch.log(torch.exp(G_i_norm[iids] @ E_d_norm.T / self.temp).sum(1) + 1e-8).mean()
-            pos_score = (torch.clamp((G_u_norm[uids] * E_g_norm[uids]).sum(1) / self.temp, -5.0, 5.0)).mean() + (
-                torch.clamp((G_i_norm[iids] * E_d_norm[iids]).sum(1) / self.temp, -5.0, 5.0)).mean()
+            neg_score = torch.log(torch.exp(G_g_norm[uids] @ E_g_norm.T / self.temp).sum(1) + 1e-8).mean()
+            neg_score += torch.log(torch.exp(G_d_norm[iids] @ E_d_norm.T / self.temp).sum(1) + 1e-8).mean()
+            pos_score = (torch.clamp((G_g_norm[uids] * E_g_norm[uids]).sum(1) / self.temp, -5.0, 5.0)).mean() + (
+                torch.clamp((G_d_norm[iids] * E_d_norm[iids]).sum(1) / self.temp, -5.0, 5.0)).mean()
             loss_s = -pos_score + neg_score
 
             # bpr loss
-            u_emb = self.E_g[uids]
+            g_emb = self.E_g[uids]
             pos_emb = self.E_d[pos]
             neg_emb = self.E_d[neg]
-            pos_scores = (u_emb * pos_emb).sum(-1)
-            neg_scores = (u_emb * neg_emb).sum(-1)
+            pos_scores = (g_emb * pos_emb).sum(-1)
+            neg_scores = (g_emb * neg_emb).sum(-1)
             loss_r = -(pos_scores - neg_scores).sigmoid().log().mean()
 
             # reg loss
